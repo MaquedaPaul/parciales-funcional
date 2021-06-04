743de2005449d06cefd5793e1b129e87af6d8bf1
Maqueda Paul <maquedapablodavid@gmail.com>
2021-06-04 00:01:59 -0300

    Revisión punto 0

--- src/Thanos.hs
+++ src/Thanos.hs
@@ -15,18 +15,14 @@ data Personaje = UnPersonaje {
 
 data Guantelete = UnGuantelete {
     material :: String,
-    gemas :: Int
-} deriving (Show,Eq)
+    gemas :: [Gema]
+} deriving (Show)
 
 data Universo = UnUniverso {
     personajes :: [Personaje]
 } deriving (Show,Eq)
 
 type Gema = Personaje -> Personaje
-
-
-
-
 
 ironMan = UnPersonaje{
     nombre = "Tony Stark",
@@ -74,6 +70,15 @@ wolverine = UnPersonaje{
 
 
 universoNormal = UnUniverso [ironMan,drStrange,wolverine,groot,laViudaNegra] 
+chasquear :: Guantelete -> Universo -> Universo
+chasquear guantelete universo
+ |puedeChasquear guantelete = chasquidoUniverso universo
+ |otherwise= universo
+puedeChasquear :: Guantelete -> Bool
+puedeChasquear guantelete = (material guantelete) == "uru" && estaCompleto guantelete
+
+estaCompleto :: Guantelete -> Bool
+estaCompleto = (==6).length.gemas 
 
 chasquidoUniverso :: Universo -> Universo
 chasquidoUniverso universo = universo {personajes = mitadDePersonajesUniverso universo} 
@@ -103,6 +108,7 @@ gemaEspacio planetaAEnviar personaje = personaje{planeta= planetaAEnviar, energi
 gemaPoder :: Gema
 gemaPoder personaje = personaje {energia=0,habilidad = efectoGemaPoder personaje}
 
+
 efectoGemaPoder :: Personaje -> [Habilidad]
 efectoGemaPoder personaje 
  |tieneMenosDosHabilidades personaje = []
@@ -127,5 +133,28 @@ esMenor = (<18).(`div` 2).edad
 gemaLoca :: Gema -> Gema
 gemaLoca gema = gema.gema
 
---usarGuantelete gema persona = undefined
+guanteleteDeGoma = UnGuantelete{
+    material = "Goma",
+    gemas = [gemaTiempo,gemaAlma "usar Mjolnir",gemaLoca (gemaAlma "programacion en Haskell")]
+}
+gemasQueEncontreAbajoDeMiCama = [gemaPoder,gemaEspacio "Marte",gemaMente 50,gemaTiempo,gemaAlma "usar Mjolnir",gemaLoca (gemaAlma "programacion en Haskell")]
+
+utilizar :: [Gema] -> Personaje -> Personaje
+utilizar gemas enemigo = foldl (usarGema) enemigo gemas
+
+usarGema :: Personaje -> Gema -> Personaje
+usarGema personaje gema  = gema personaje
+
+--hola @Pablo. en primer lugar cuando te dicen "efecto de lado", es lo mismo que "efecto colateral" o directamente "efecto" que es el término que normalmente usamos
+
+-- En funcional estuvimos trabajando sin efecto, los datos son inmutables por ende no hay algo que podamos hacer para realmente cambiar a la víctima, sin embargo eso no impide que se pueda cumplir con el requerimiento que se pide en ese punto. Entonces cómo es que se logra ejecutar el poder de cada una de las gemas sobre la víctima, si no se pueden cambiar los datos?
+
+-- Si no se pueden cambiar los datos, se van creando nuevas víctimas que tienen los campos afectados según la gema que se utilizó.
+-- Por cada gema utilizada en una víctima, creo una más y a esa le aplicó la gema siguiente en la lista hasta terminar
+
+--gemaMasPoderosa :: Guantelete -> Personaje -> Gema
+--gemaMasPoderosa (UnGuantelete _ (gema:gemas)) personaje
+-- --|
+--- |otherwise
+
 

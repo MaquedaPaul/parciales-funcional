7e970339cf2ed6d8e230815fc904b5055b7d6658
Maqueda Paul <maquedapablodavid@gmail.com>
2021-06-07 20:45:27 -0300

    Punto 3) y comienzo 4) [Piratas.hs]

--- consigas/piratas.md
+++ consigas/piratas.md
@@ -14,8 +14,33 @@ Beber grog: aumenta la energía en 20. También funciona con tripulantes muertos
 a) Modelar las actividades diarias, teniendo en cuenta que existen más de los ejemplos mencionados.
 b) Queremos saber si un tripulante está muerto. Esto ocurre cuando se queda sin energía.
 
+Existen 3 tipos de barcos de diferente tamaño y por lo tanto diferente nivel de maniobrabilidad ya que mientras más grandes, más velas, sogas y bueno… demás artilugios. Los barcos son:
+
+Los Galeones, monstruosos navíos de 150 m2,
+
+los Bergantines versátiles construcciones de 100 m2, 
+
+y finalmente el pequeño, viejo y confiable Balandro de 50 m2.
+
+Queremos:
+a) Saber si un barco es un barco fantasma, que ocurre cuando no tiene tripulación o toda su tripulación murió.
+
+b) Llenar un barco a su máxima capacidad. Cuando esto sucede el barco queda con 7 kg de oro por metro cuadrado, y 3 balas por persona.
 
 
+
+Se sabe que cuando el navío se enfrenta a un barco enemigo pasa lo siguiente: 
+
+Si es más grande que su enemigo, gana si tiene igual o más balas.
+
+Si ambos tienen el mismo tamaño, le gana si tiene igual o más madera.
+
+Si es más chico que el enemigo, le gana si tiene tantos o más tripulantes con vida.
+
+Si el barco pierde contra su oponente se queda sin recursos. En caso contrario, se lleva consigo todos los recursos del otro barco festejando con grog! muuuuuucho grog!
+
+3)
+Queremos saber cómo quedó el barco luego de enfrentarse con un enemigo. 
 
 
 
--- src/Piratas.hs
+++ src/Piratas.hs
@@ -16,7 +16,8 @@ data Barco = UnBarco {
     tripulacion :: [Tripulante],
     oro :: Int,
     balas :: Int,
-    dimension :: Int
+    dimension :: Int,
+    madera :: Int
 } deriving (Show,Eq)
 
 
@@ -29,6 +30,20 @@ carlos = UnTripulante {
     nombre = "Carlos Malasangre",
     energia = 40
 } 
+
+jorge = UnTripulante {
+    nombre = "Jorge el Malhabido",
+    energia = 30
+} 
+
+ramon = UnTripulante {
+    nombre = "Ramon Ramonete",
+    energia = 70
+} 
+
+
+
+
 
 modificarEnergia ::  Int -> Tripulante -> Tripulante
 modificarEnergia delta tripulante = tripulante {energia= energia tripulante + delta}
@@ -57,27 +72,40 @@ galeon = UnBarco {
     tripulacion = [],
     oro = 0,
     balas = 0,
-    dimension = 150
+    dimension = 150,
+    madera = 0
 }
 bergantin = UnBarco {
     tripulacion = [],
     oro = 0,
     balas = 0,
-    dimension = 100
+    dimension = 100,
+    madera = 0
 }
 balandro = UnBarco {
     tripulacion = [],
     oro = 0,
     balas = 0,
-    dimension = 50
+    dimension = 50,
+    madera = 0
 }
 
 galeonDeJeff = UnBarco {
     tripulacion = [jeff,carlos],
-    oro = 0,
-    balas = 0,
-    dimension = 150
+    oro = 20,
+    balas = 20,
+    dimension = 150,
+    madera = 20
 }
+
+barcoMalandrines = UnBarco {
+    tripulacion = [jorge,ramon],
+    oro = 10,
+    balas = 40,
+    dimension = 100,
+    madera = 10
+}
+
 
 esBarcoFantasma :: Barco -> Bool
 esBarcoFantasma barco = all (estaMuerto) (tripulacion barco) || null(tripulacion barco)
@@ -90,9 +118,85 @@ llenarBarcoMax barco = barco
  {oro = 7* dimension barco, balas= cantidadTripulantes barco*3} 
 
 
+type Enfrentamiento = Barco -> Barco -> Barco
+
+enfrentamientoBarcos :: Enfrentamiento
+enfrentamientoBarcos barco enemigo
+ |esMasGrande barco enemigo && tieneBalas barco enemigo = ganaEncuentro barco enemigo
+ |mismoTamano barco enemigo && tieneComponenteBarco madera barco (>=) enemigo = ganaEncuentro barco enemigo
+ |esMasPequeno barco enemigo && tieneComponenteBarco (length.tripulacion) barco (>=) enemigo = ganaEncuentro barco enemigo
+ |otherwise = pierdeEncuentro barco
+
+ganaEncuentro :: Enfrentamiento
+ganaEncuentro barcoGanador barcoPerdedor = barcoGanador {
+oro = saquear oro barcoGanador barcoPerdedor,
+madera = saquear madera barcoGanador barcoPerdedor,
+balas = saquear balas barcoGanador barcoPerdedor
+}
+pierdeEncuentro :: Barco -> Barco
+pierdeEncuentro barco = barco {oro = 0,
+madera = 0,
+balas = 0
+}
+
+
+saquear :: (Barco->Int) -> Barco -> Barco -> Int
+saquear elementoSaqueado barco enemigo = (elementoSaqueado barco) + (elementoSaqueado enemigo)
+
+
+
 -- data Barco = UnBarco {
 --     tripulacion :: [Tripulante],
 --     oro :: Int,
 --     balas :: Int,
---     dimension :: Int
+--     dimension :: Int,
+--     madera :: Int
 -- } deriving (Show,Eq)
+
+
+
+esMasGrande :: Barco -> Barco -> Bool
+esMasGrande barco enemigo = (dimension barco) > (dimension enemigo)
+
+tieneBalas :: Barco -> Barco -> Bool
+tieneBalas barco enemigo = (balas barco) >= (balas enemigo)
+----------------------------------------------------------------
+
+
+mismoTamano :: Barco -> Barco -> Bool
+mismoTamano barco enemigo = (dimension barco) == (dimension enemigo)
+------------------------------------------------------------------------
+
+esMasPequeno :: Barco -> Barco -> Bool
+esMasPequeno barco enemigo = (dimension barco) < (dimension enemigo)
+------------------------------------------------------------------------
+--Se puede parametrizar pero se pierde declaratividad
+
+analizarTamano :: Barco -> (Int -> Int -> Bool) -> Barco -> Bool
+analizarTamano barco parametro enemigo = (dimension barco) `parametro` (dimension enemigo)
+
+tieneComponenteBarco :: (Barco -> Int) -> Barco -> (Int -> Int -> Bool) -> Barco -> Bool
+tieneComponenteBarco componente barco parametro enemigo = (componente barco) `parametro` (componente enemigo)
+
+---------------------------------------------------------
+
+type Suceso = Barco -> Barco
+
+embarcarTesoro :: Int -> Suceso
+embarcarTesoro pesoEnOro barco = barco {
+    tripulacion = (flip dividirRepartir (tripulacion barco)). cuantosNoMueren (transportarUnaCarga pesoEnOro) $ tripulacion barco,
+    oro = pesoEnOro
+    }
+
+cuantosNoMueren :: Actividad -> [Tripulante] ->Int
+cuantosNoMueren actividad = length.(filter (not.estaMuerto)).(map actividad)
+
+dividirRepartir :: Int -> [Tripulante] -> [Tripulante]
+dividirRepartir peso = map (transportarUnaCarga peso) 
+
+
+modificarOroBarco :: Int -> Barco -> Barco
+modificarOroBarco deltaOro barco = barco {oro = oro barco + deltaOro}
+
+
+

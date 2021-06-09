a568d1e27025067402bd4905e01c6cc43a7b7b54
Maqueda Paul <maquedapablodavid@gmail.com>
2021-06-08 21:05:50 -0300

    Punto 3 [FINALIZADO][Barbaros.hs]

--- src/Barbaros.hs
+++ src/Barbaros.hs
@@ -53,3 +53,75 @@ megafono barbaro = barbaro {habilidades = [map toUpper . concat . habilidades $ 
 
 megafonoBarbarico :: Objeto
 megafonoBarbarico = cuerda ardilla megafono
+
+type Evento = Barbaro -> Bool
+type Aventura = [Evento]
+
+type GrupoBarbaros = [Barbaro]
+
+aventuraBarbarica :: Aventura
+aventuraBarbarica = [invasionDeSuciosDuendes,cremalleraDelTiempo,ritualDeFechorias]
+
+invasionDeSuciosDuendes :: Evento
+invasionDeSuciosDuendes = sobreviveSi any (== "Escribir Poesia Atroz").habilidades
+
+cremalleraDelTiempo :: Evento
+cremalleraDelTiempo = sobreviveSi flip elem (noTienePulgares).nombre
+
+noTienePulgares :: [String]
+noTienePulgares = ["Faffy","Astro"]
+
+sobreviveSi operacion condicion atributo = operacion (condicion) atributo
+
+ritualDeFechorias :: Evento 
+ritualDeFechorias barbaro = any (flip pasaPrueba barbaro) [saqueo,gritoDeGuerra,caligrafia] --sobreviveSi
+
+type Prueba = Barbaro -> Bool
+
+pasaPrueba :: Prueba -> Barbaro -> Bool
+pasaPrueba prueba = prueba
+
+saqueo :: Prueba
+saqueo barbaro = (tieneHabilidad "saquear" barbaro) && (tieneFuerza 80 barbaro)
+
+tieneHabilidad :: String -> Barbaro -> Bool
+tieneHabilidad habilidad = any (==habilidad).habilidades 
+
+tieneFuerza :: Int -> Barbaro -> Bool
+tieneFuerza fuerzaRequerida = (>=fuerzaRequerida).fuerza
+
+gritoDeGuerra :: Prueba
+gritoDeGuerra barbaro = ((*4).cantidadObjetos $ barbaro) < (poderGritoGuerra barbaro)
+
+cantidadObjetos :: Barbaro -> Int
+cantidadObjetos = length.objetos
+
+poderGritoGuerra :: Barbaro -> Int
+poderGritoGuerra = length.concat.habilidades.megafono
+
+caligrafia :: Prueba
+caligrafia = cumpleEstandarBarbarico.habilidades
+
+cumpleEstandarBarbarico :: [String] -> Bool
+cumpleEstandarBarbarico habilidades = (all comienzaConMayuscula habilidades) && (all masDeTresVocales habilidades)
+
+comienzaConMayuscula :: String -> Bool
+comienzaConMayuscula palabra = ((==).head $ palabra).toUpper $ head palabra 
+
+masDeTresVocales :: String -> Bool
+masDeTresVocales = (>3).length
+
+vocales :: String -> String
+vocales palabra = filter (esVocal) palabra
+
+esVocal :: Char -> Bool
+esVocal caracter = elem (caracter) ['a','e','i','o','u']
+
+sobrevivientes :: [Barbaro] -> Aventura -> [Barbaro]
+sobrevivientes barbaros aventura = filter (flip sobreviveAventura aventura) barbaros
+
+sobreviveAventura :: Barbaro -> Aventura -> Bool
+sobreviveAventura barbaro aventura = all (sobreviveEvento barbaro) aventura
+
+sobreviveEvento :: Barbaro -> Evento -> Bool
+sobreviveEvento barbaro evento = evento barbaro 

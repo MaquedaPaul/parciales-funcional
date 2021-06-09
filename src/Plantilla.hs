c7222eb95a5a3485780b099bb5fc4456b9f787d9
Maqueda Paul <maquedapablodavid@gmail.com>
2021-06-09 14:00:21 -0300

    Plantilla añadida

---
+++ src/Plantilla.hs
@@ -0,0 +1,17 @@
+-- module Barbaros where
+
+-- import Text.Show.Functions
+-- import Data.Char
+-- import Data.List
+
+-- data Barco = UnBarco {
+--     tripulacion :: [Tripulante],
+--     oro :: Int,
+--     balas :: Int,
+--     dimension :: Int,
+--     madera :: Int
+-- } deriving (Show,Eq)
+
+
+
+

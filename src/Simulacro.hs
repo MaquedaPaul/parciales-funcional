f940c8f144f0a8e8f1802b6d3932728fba66b47a
Maqueda Paul <maquedapablodavid@gmail.com>
2021-06-09 02:34:53 -0300

    Seteo del simulacro para comenzar a realizarlo

---
+++ src/Simulacro.hs
@@ -0,0 +1,27 @@
+module Pdeppi where
+
+import Text.Show.Functions
+import Data.Char
+import Data.List
+
+-- type Habilidad = String
+
+-- data Barco = UnBarco {
+--     tripulacion :: [Tripulante],
+--     oro :: Int,
+--     balas :: Int,
+--     dimension :: Int,
+--     madera :: Int
+-- } deriving (Show,Eq)
+
+-- data Barbaro = UnBarbaro {
+--     nombre :: String,
+--     fuerza :: Int,
+--     habilidades :: [Habilidad],
+--     objetos :: [Objeto]
+-- } deriving (Show)
+
+
+
+
+

c1202c794772ed263bd3d62b7a230634d07d0c56
juampibruno <jubruno@frba.utn.edu.ar>
2021-06-13 16:45:10 -0300

    Parcial Casa papel casi completo faltan puntos 8 y 9

--- src/CasaPapel.hs
+++ src/CasaPapel.hs
@@ -14,41 +14,40 @@ data Ladron = UnLadron {
     armas :: [Arma]
 } deriving (Show)
 
-type Plan = String
+
 
 data Rehen = UnRehen {
     nombreRehen :: String,
     nivelComplot :: Int,
     nivelMiedo :: Int,
-    plan :: Plan
-} deriving (Show,Eq)
+    plan :: [Plan]
+} deriving (Show)
 
 
 tokio = UnLadron {
     nombreLadron = "Tokio",
     habilidades = ["trabajo psicologico","entrar en moto"],
-    armas = [pistola 30,pistola 15]
+    armas = [pistola 9,pistola 9,ametralladora 30]
 }
 profesor = UnLadron {
     nombreLadron = "Profesor",
     habilidades = ["disfrazarse de linyera","disfrazarse de payaso","estar siempre un paso adelante"],
-    armas = [pistola 30]
+    armas = []
 }
-
 
 pablo = UnRehen {
     nombreRehen = "Pablo",
     nivelComplot = 40,
-    nivelMiedo = 40,
-    plan = "Esconderse"
+    nivelMiedo = 30,
+    plan = [esconderse]
 }
 
 arturito = UnRehen {
     nombreRehen = "Arturo",
     nivelComplot = 70,
     nivelMiedo = 50,
-    plan = "Esconderse y luego atacar con Pablo"
-}
+    plan = [esconderse, atacarAlLadron pablo]
+    }
 
 
 
@@ -86,8 +85,10 @@ mitadComplot = (`div`2).nivelComplot
 --Disparos: disparar al techo como medida disuasiva. Se usa el arma que le genera más miedo al rehén intimidado
 
 
-disparos :: [Arma] -> Rehen -> Rehen
-disparos unasArmas rehen = undefined
+type Intimidar = Ladron -> Rehen -> Rehen
+
+disparos :: Intimidar
+disparos unLadron rehen = armaQueLeGeneraMasMiedo rehen (ladronArmas unLadron)
 
 
 armaQueLeGeneraMasMiedo ::  Rehen -> [Arma] -> Rehen
@@ -139,10 +140,10 @@ En otros casos, el miedo del rehén sube en 10.
 
 -}
 
--- hacerseElMalo :: Ladron -> Rehen -> Rehen
--- hacerseElMalo (Ladron "Berlin" _ unasHabilidades) rehen = modificarMiedoRehen (lenght.concat $ unasHabilidades) 
--- hacerseElMalo (Ladron "Rio" _ _ ) rehen = modificarComplotRehen 20 rehen
--- hacerseElMalo (Ladron _ _ _) rehen = modificarMiedoRehen 10 rehen
+-- hacerseElMalo :: Intimidar
+-- hacerseElMalo (UnLadron "Berlin" _ unasHabilidades) rehen = modificarMiedoRehen (lenght.concat $ unasHabilidades) 
+-- hacerseElMalo (UnLadron "Rio" _ _ ) rehen = modificarComplotRehen 20 rehen
+-- hacerseElMalo (UnLadron _ _ _) rehen = modificarMiedoRehen 10 rehen
  
 -- Ctrl + K + C
 -- Ctrl + K + U
@@ -150,12 +151,89 @@ En otros casos, el miedo del rehén sube en 10.
 -- Ctrl + Shift + Flecha
 -- Ctrl + Flecha arriba
 
+--A los rehenes no les gusta ser rehenes, por eso intentan rebelarse contra los ladrones, siempre que tengan más complot que miedo, 
+--ideando planes como:
+
 -- Atacar al ladrón: le quita tantas armas como la cantidad de letras del nombre de su compañero de ataque, dividido por 10.
 
+type Plan = Ladron -> Ladron
+
+ladronArmas :: Ladron -> [Arma]
+ladronArmas = armas
+
+quitarArmasLadron :: Int -> Ladron -> Ladron
+quitarArmasLadron cantidadSacar unLadron = unLadron {armas = take (min cantidadSacar (length.armas $unLadron)) (armas unLadron)}
 
 
+atacarAlLadron ::  Rehen -> Plan
+atacarAlLadron compañeroAtaque unLadron = quitarArmasLadron (flip div 10.length.nombreRehen $compañeroAtaque) unLadron
+
+--Esconderse: Hace que un ladrón pierda una cantidad de armas igual a su cantidad de habilidades dividido 3. 
+
+cantidadHabilidadesLadron :: Ladron -> Int
+cantidadHabilidadesLadron unLadron = length.habilidades $unLadron
+
+esconderse :: Plan
+esconderse unLadron = quitarArmasLadron (div (cantidadHabilidadesLadron unLadron)  3) unLadron
 
 
+--  2-  Saber si un ladrón es inteligente. Ocurre cuando tiene más de dos habilidades, además el Profesor es la mente maestra, 
+--  por lo que indudablemente es inteligente.
 
+esInteligente :: Ladron -> Bool
+esInteligente unLadron = ((>2).cantidadHabilidadesLadron $unLadron) || ((== "Profesor").nombreLadron $unLadron)
 
+-- 3 Que un ladrón consiga un arma nueva, y se la agregue a las que ya tiene.
 
+consigueArmaNueva :: Ladron -> Arma -> Ladron
+consigueArmaNueva unLadron unArma = unLadron {armas = unArma : armas unLadron}
+
+--4 Que un ladrón intimide a un rehén, usando alguno de los métodos planeados.
+
+-- 5 Que un ladrón calme las aguas, disparando al techo frente a un grupo de rehenes, 
+--de los cuales se calman los que tengan más de 60 de complot.
+
+complotRehenMayorA :: Int -> Rehen -> Bool
+complotRehenMayorA numero rehen = (> numero).nivelComplot $rehen
+
+calmeLasAguas :: Ladron -> [Rehen] -> [Rehen]
+calmeLasAguas ladron unosRehenes = filter(complotRehenMayorA 60) . map (disparos ladron) $unosRehenes 
+
+-- 6-Saber si un ladrón puede escaparse de la policía. Esto se cumple cuando alguna de las habilidades del ladrón empieza con “disfrazarse de”.
+
+empiezaConDisfrazarseDe :: Habilidad -> Bool
+empiezaConDisfrazarseDe unaHabilidad = (== "disfrazarse de") . take 14 $ unaHabilidad
+
+puedeEscaparse :: Ladron -> Bool
+puedeEscaparse ladron = any (empiezaConDisfrazarseDe) (habilidades ladron)
+
+{-
+    7-Saber si la cosa pinta mal, que es cuando dados unos ladrones y unos rehenes,
+     el nivel de complot promedio de los rehenes es mayor al nivel de miedo promedio multiplicado por la cantidad de armas de los ladrones.
+-}
+
+--sumarNivelComplot :: [Rehen] -> Int
+--sumarNivelComplot unosRehenes = sum.map (nivelComplot) $unosRehenes
+
+nivelComplotPromedio :: [Rehen] -> Int
+nivelComplotPromedio unosRehenes = div (sum.map (nivelComplot) $unosRehenes) (length unosRehenes)
+
+nivelMiedoPromedio :: [Rehen] -> Int
+nivelMiedoPromedio unosRehenes = div (sum.map (nivelMiedo) $unosRehenes) (length unosRehenes)
+
+cantidadArmasLadrones :: [Ladron] -> Int
+cantidadArmasLadrones unosLadrones = length.concat.map (armas) $unosLadrones
+
+laCosaPintaMal :: [Ladron] -> [Rehen] -> Bool
+laCosaPintaMal unosLadrones unosRehenes = nivelComplotPromedio unosRehenes > (nivelMiedoPromedio unosRehenes * cantidadArmasLadrones unosLadrones)
+
+--8 -Que los rehenes se rebelen contra un ladrón, usando el plan que tengan en mente. 
+--Saben que es mala idea, por lo que todos pierden 10 de complot antes de comenzar la rebelión.
+
+-- 9 Necesito resolver el 8 para poderlo hacer
+
+--10 No se puede ya que un ladron al contar con una cantidad infinita de armas no terminaria nunca de multiplicar
+
+--11 Hay 2 respuestas posibles
+-- si solamente dentro de los planes de los ladrones esta atacarAlLadron si se puede ya que no me interesan las habilidades
+-- Pero en cambio si dentro de la lista de planes esta esconderse no se va a poder ya que al ser infinito el programa se me queda colgado
